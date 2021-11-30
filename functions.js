47f9a2e3c9beffdd79e13cde322c0c90bd6721b4
jiwonkim19 <jiwonkim89@gmail.com>
2021-11-30 14:47:35 -0800

    init commit

---
+++ functions.js
@@ -0,0 +1,27 @@
+const isEven = (num) => {
+    return num % 2 === 0    
+}
+
+// const isEven = (num) => {
+//     if (num % 2 === 0) {
+//        return true 
+//     } else {
+//         return false
+//     }
+// }
+
+console.log(isEven(3))
+console.log(isEven(4))
+console.log(isEven(0))
+console.log(isEven(-2))
+console.log(isEven(-3))
+
+// const checkEven = (num) => {
+//     if (isEven(num)) {
+//         console.log('is even')
+//     } else {
+//         console.log('is odd')
+//     }
+// }
+// checkEven(3)
+
---
+++ lint project.js
@@ -0,0 +1,7 @@
+Using for...in, iterate through the spaceship.crew object in 
+the code editor and console.log() a list of crew roles and names in the following format: 
+'[crew member's role]: [crew member's name]', e.g.,'chief officer: Dan'. 
+
+for (let crewRole in spaceship.crew) {
+    console.log (spaceship.crew.crewRole + ':' spaceship.crew.crewRole.name)
+}
